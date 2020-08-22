e0450e9cad99538ecfdbe2479be6e0c661ceeece
Guille Paz <guillermo.paz@mercadolibre.com>
2020-08-22 14:22:46 -0300

    Improve errors hanlder.

--- dev/app.js
+++ dev/app.js
@@ -3,6 +3,11 @@ const webVitals = require('../api/web-vitals');
 
 const app = express();
 
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  next();
+});
+
 app.get('/favicon.ico', (req, res) => res.status(204).end());
 app.get('/api/web-vitals', webVitals);
 
--- utils/fetch-perf-metrics.js
+++ utils/fetch-perf-metrics.js
@@ -11,29 +11,40 @@ const metrics = {
 
 async function fetchVitalsFromCrux(from, site, body) {
   return axios.post(API_URL, { ...body, [from]: site }, { params: query })
-    .catch(err => { throw err.response.data.error; });
+    .catch(err => { throw { url: site, error: err.response.data.error }; });
 }
 
 async function fetchPerformanceMetrics(from, sites, body) {
   const promises = sites.map(site => fetchVitalsFromCrux(from, site, body));
-  return Promise.all(promises)
+  return Promise.allSettled(promises)
     .then(response => {
-      const results = response.map(result => ({
-        [from]: result.data.record.key[from],
-        metrics: Object.fromEntries(
-          Object.entries(result.data.record.metrics)
-            .map(metric => {
-              metric[1].value = metric[1].percentiles.p75;
-              delete metric[1].percentiles;
-              metric[1].histogram = metric[1].histogram.map(h => {
-                h.density = parseFloat((h.density * 100).toFixed(2));
-                return h;
-              });
-              metric[0] = metrics[metric[0]];
-              return metric;
-            })
-        ),
-      }));
+      const results = response.map(result => {
+        if (result.status === 'fulfilled') {
+          const data = result.value.data;
+          return {
+            [from]: data.record.key[from],
+            metrics: Object.fromEntries(
+              Object.entries(data.record.metrics)
+                .map(metric => {
+                  metric[1].value = metric[1].percentiles.p75;
+                  delete metric[1].percentiles;
+                  metric[1].histogram = metric[1].histogram.map(h => {
+                    h.density = parseFloat((h.density * 100).toFixed(2));
+                    return h;
+                  });
+                  metric[0] = metrics[metric[0]];
+                  return metric;
+                })
+            ),
+          };
+        } else if (result.status === 'rejected') {
+          return {
+            [from]: result.reason.url,
+            metrics: null,
+            error: result.reason.error.message,
+          };
+        }
+      });
       return results;
     })
     .catch(err => {
